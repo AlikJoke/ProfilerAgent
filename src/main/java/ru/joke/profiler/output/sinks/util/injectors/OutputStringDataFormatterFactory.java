5a619bab0ba7ef7d84ac22d86001bcea1731d264
Alik <typicaljprogrammer@gmail.com>
2025-01-10 14:28:38 +0300

    fix SpanId/Parent SpanId

--- src/main/java/ru/joke/profiler/output/sinks/util/injectors/OutputStringDataFormatterFactory.java
+++ src/main/java/ru/joke/profiler/output/sinks/util/injectors/OutputStringDataFormatterFactory.java
@@ -2,7 +2,7 @@ package ru.joke.profiler.output.sinks.util.injectors;
 
 public final class OutputStringDataFormatterFactory {
 
-    private static final String DEFAULT_FORMAT = "${current_ts} [${source},${trace_id},${parent_span},${span},${depth}] --- [${thread}] --- ${method} : ${enter_ts} : ${elapsed}";
+    private static final String DEFAULT_FORMAT = "${current_ts} [${source},${trace_id},${parent_span_id},${span_id},${depth}] --- [${thread}] --- ${method} : ${enter_ts} : ${elapsed}";
 
     public static OutputStringDataFormatter create(final String pattern) {
         return new OutputStringDataFormatter(
